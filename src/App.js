fd942e86c3c120ce3c1ad6686f93cd1305564c9a
musturi <rakesh.musturi.ext@nokia.com>
2020-07-27 15:41:19 +0530

    release code

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+import Notification from './components/Notification'
+
+function App() {
+  const listItems = [
+      {
+        UTC: "1408648665",
+        list: [
+          {
+            type: "Message",
+            content: "A messgae description for testing notofication bar",
+            count: 3,
+            timestamp: "1PM"
+          }
+        ]
+      },
+      {
+        UTC: "1598103780",
+        list: [
+          {
+            type: "Login",
+            content: "A messgae description for testing notofication bar",
+            count: 1
+          }
+        ]
+      },
+      {
+        UTC: "1595594400",
+        list: [
+          {
+            type: "Login",
+            content: "A messgae description for testing notofication bar",
+            count: 4
+          }
+        ]
+      },
+      {
+        UTC: "1595575200",
+        list: [
+          {
+            type: "Critical",
+            content: "A messgae description for testing notofication bar",
+            count: 3
+          }
+        ]
+      }
+    ];
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Notification listItems={listItems} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
